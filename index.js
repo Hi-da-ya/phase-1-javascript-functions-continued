a3ae3a69bd47e14f275c3776ccf9360ab9303079
Hidaya Wetsakhulia <hidayawetsa@gmail.com>
2024-03-28 23:50:28 +0300

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+//function declaration>>>>>>>
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`; 
+};
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+};
+
+   function wrapAdjective(highlight = "*"){
+    return function(emphatic = "special"){
+        return `You are ${highlight}${emphatic}${highlight}!`;
+    };
+   }; 
+    
